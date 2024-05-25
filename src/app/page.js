c7d91d986ec93607ff7848d71d96ea156574dc3d
manoj98gopal <manoj98gopal@gmail.com>
2024-05-25 14:15:20 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-center text-3xl">Loading...</h1>
+      <h1 className="text-center text-3xl">Loading... updated</h1>
     </div>
   );
 }
